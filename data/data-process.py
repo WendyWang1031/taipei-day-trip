32f7d825edafa7dcc2a4b478f87a07cba650850d
WendyWang1031 <w5569590@gmail.com>
2024-05-28 11:19:20 +0800

    fix data-process.py and fix column name

--- data/data-process.py
+++ data/data-process.py
@@ -2,11 +2,6 @@ import json
 import pymysql
 import re
 
-with open('data/taipei-attractions.json') as data_details:
-    data = json.load(data_details)
-
-data_results = data["result"]["results"]
-
 
 
 
@@ -20,11 +15,61 @@ db =  pymysql.connect(
 
 cursor = db.cursor()
 
-insert_location_sql = """insert into location(_id,name,MRT,SERIAL_NO,address,RowNumber,rate,direction,
-date,avBegin,avEnd,longitude,latitude,REF_WP,langinfo,CAT,MEMO_TIME,POI,idpt,description)
+create_location_sql = """
+CREATE TABLE IF NOT EXISTS location(
+    id BIGINT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    mrt VARCHAR(255),
+    SERIAL_NO VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    RowNumber VARCHAR(255) NOT NULL,
+    langinfo VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    rate BIGINT NOT NULL,
+    transport TEXT NOT NULL,
+    date DATETIME NOT NULL,
+    avBegin DATETIME NOT NULL,
+    avEnd DATETIME NOT NULL,
+    lng DECIMAL NOT NULL,
+    lat DECIMAL NOT NULL,
+    REF_WP VARCHAR(255) NOT NULL,
+    MEMO_TIME VARCHAR(255),
+    POI VARCHAR(255) NOT NULL,
+    idpt VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+"""
+
+create_url_file_sql = """
+CREATE TABLE IF NOT EXISTS URL_file(
+    file_id BIGINT NOT NULL AUTO_INCREMENT,
+    location_id BIGINT NOT NULL,
+    images VARCHAR(255) NOT NULL,
+    PRIMARY KEY (file_id),
+    FOREIGN KEY (location_id) REFERENCES location(id)
+);
+"""
+
+try:
+    cursor.execute(create_location_sql)
+    cursor.execute(create_url_file_sql)
+    db.commit()
+except Exception as e :
+    print("Error creating tables:" , e)
+    db.rollback()
+
+
+with open('data/taipei-attractions.json') as data_details:
+    data = json.load(data_details)
+
+data_results = data["result"]["results"]
+
+insert_location_sql = """insert ignore into location(id,name,mrt,SERIAL_NO,address,RowNumber,rate,transport,
+date,avBegin,avEnd,lng,lat,REF_WP,langinfo,category,MEMO_TIME,POI,idpt,description)
 values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
 
-insert_URL_file_sql = """insert into URL_file( location_id , url)
+insert_URL_file_sql = """insert ignore into URL_file( location_id , images)
 values(%s,%s)
 """
 
