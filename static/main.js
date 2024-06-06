eba2a59c3d7efb3ebd1869f6d9675457707731ad
WendyWang1031 <w5569590@gmail.com>
2024-06-06 09:26:16 +0800

    fix multiple sroll showing pages problem using setTimeout

--- static/main.js
+++ static/main.js
@@ -104,14 +104,27 @@ async function fetchAttractions(
 }
 fetchAttractions();
 
+function throttle(func, limit) {
+  let inThrottle;
+  return function () {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
 const observer = new IntersectionObserver(
-  (entries) => {
+  throttle((entries) => {
     const firstEntry = entries[0];
-    console.log("firstEntry:", firstEntry);
+
     if (firstEntry.isIntersecting && hasNextPage && !isLoading) {
       fetchAttractions("", currentPage + 1);
     }
-  },
+  }, 200),
   { threshold: 0.5 }
 );
 
