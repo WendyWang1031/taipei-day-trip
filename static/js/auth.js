30c3573320799e9225aa56f35fe75c26ed63c355
WendyWang1031 <w5569590@gmail.com>
2024-06-18 11:05:13 +0800

    seperate main.js to auth.js from login and signinfunc

---
+++ static/js/auth.js
@@ -0,0 +1,45 @@
+const loginSigninBtn = document.querySelector(".login-signin");
+const closeSigninBtn = document.querySelector(".close-sigin");
+const gotoSignupBtn = document.querySelector(".go-to-signup");
+const closeSignupBtn = document.querySelector(".close-sigup");
+const gotoSigninBtn = document.querySelector(".go-to-signin");
+const signinMask = document.querySelector(".signin-mask");
+const signupMask = document.querySelector(".signup-mask");
+
+signinMask.style.display = "none";
+signupMask.style.display = "none";
+
+// 各種功能性的函數呼叫
+
+loginSigninBtn.addEventListener("click", loginSignin);
+closeSigninBtn.addEventListener("click", closeSignin);
+gotoSignupBtn.addEventListener("click", gotoSignup);
+closeSignupBtn.addEventListener("click", closeSignup);
+gotoSigninBtn.addEventListener("click", gotoSignin);
+
+function loginSignin(event) {
+  event.preventDefault();
+  signinMask.style.display = "flex";
+}
+
+function closeSignin(event) {
+  event.preventDefault();
+  signinMask.style.display = "none";
+}
+
+function gotoSignup(event) {
+  event.preventDefault();
+  signinMask.style.display = "none";
+  signupMask.style.display = "flex";
+}
+
+function closeSignup(event) {
+  event.preventDefault();
+  signupMask.style.display = "none";
+}
+
+function gotoSignin(event) {
+  event.preventDefault();
+  signupMask.style.display = "none";
+  signinMask.style.display = "flex";
+}
