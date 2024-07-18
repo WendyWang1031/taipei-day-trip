013f256eb140b049b698bf2717352a14965c2c94
WendyWang1031 <w5569590@gmail.com>
2024-07-18 14:00:37 +0800

    TEST : add catch Error

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -59,14 +59,16 @@ async function fetchGetAttractionID(attractionId) {
     console.log("before response:");
     if (!response.ok) {
       console.log("response:");
-      window.location.href = "/";
+      // window.location.href = "/";
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
     console.log("before data:");
     const data = await response.json();
     console.log("after data:");
     if (!data || !data.data || attractionId != data.data.id) {
       console.log("in data:");
-      window.location.href = "/";
+      // window.location.href = "/";
+      throw new Error("Invalid data format or mismatched attraction ID");
     }
     console.log(data.data);
     ViewAttraction.displayAttraction(data.data);
