6151fad055e924819661b0f91a253bc523893f9b
WendyWang1031 <w5569590@gmail.com>
2024-07-18 11:54:35 +0800

    BackEnd : logging middleware add method and status code

--- middlewares/logging_middleware.py
+++ middlewares/logging_middleware.py
@@ -1,11 +1,22 @@
 import logging
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
 
 
 logging.basicConfig(level=logging.INFO , format='%(asctime)s - %(message)s' , filename= 'app.log')
 
 class LoggingMiddleware(BaseHTTPMiddleware):
-	async def dispatch(self , request , call_next):
-		logging.info(f'Request from IP: {request.client.host} to URL: {request.url.path}')
-		response = await call_next(request)
-		return response
+    EXCLUDED_IPS = ['192.168.68.1', '52.89.39.93' , '127.0.0.1' ]
+    
+    async def dispatch(self , request , call_next):
+        
+        response: Response = await call_next(request)
+        
+        if request.client.host not in self.EXCLUDED_IPS:
+            logging.info(
+                f"Request IP: {request.client.host}, Method: {request.method}, URL: {request.url.path} | "
+                f"Response Status: {response.status_code}"
+            )
+        
+        return response
